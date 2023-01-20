ff4cd2a98dff79070cd7ef49d30eba33905bd37f
Darshit <darshitbhuva9330@gmail.com>
2023-01-20 11:12:56 +0530

    Improvement in Login Functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 // import { countDocuments } from '../../../Backend/models/userModel'
-import  './Login.css'
+import './Login.css'
 
 import { useEffect } from 'react'
- function Login() {
+import { useNavigate } from 'react-router-dom'
+function Login() {
+
+  const navigate = useNavigate();
   const [credentials, setCredentials] = useState({ email: "", password: "" })
-  
-  const isLoggedin=()=>{
-    if(localStorage.getItem('username')){
-      return true
-    }
-    else{
-      return false
-    }
-  }
-	
+  const [state, setState] = useState(false);
+
+  // const isLoggedin = () => {
+  //   if (localStorage.getItem('username')) {
+  //     return true
+  //   }
+  //   else {
+  //     return false
+  //   }
+  // }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -22,75 +26,98 @@ import { useEffect } from 'react'
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: credentials.email, password: credentials.password }),
-      });
-      const json = await response.json()
-    
-      if (json.success != null) {
-        alert('Login successful');
-      }
-      else{
-        alert('Invalid Credentials');
-      }
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password }),
+    });
+    const json = await response.json()
+
+    if (json.success != null) {
+      setState(true);
+      window.scrollTo(0, 0);
+      
+      localStorage.setItem("username", json.username);
+
+      setTimeout(() => {
+        navigate("/");
+        window.location.reload(true);
+      }, 2000);
+    }
+    else {
+      alert('Invalid Credentials');
+    }
   }
   const onChange = (e) => {
 
     setCredentials({ ...credentials, [e.target.name]: e.target.value })
-    console.log(credentials.email);
-    console.log(credentials.password);
-}
+  
+  }
 
   useEffect(() => {
-    isLoggedin();
-  }, [])
+    // isLoggedin();
+  }, [state])
 
-	return (
-		<div>
-        <div style={{marginTop:'80px'}}>
-    {/* <Heading/> */}
+  return (
 
-  </div>	
-  
-  <body>
-    <div className="bg-img">
-      <div className="content">
-        <header style={{color:'black'}}>Login</header>
-        <form onSubmit={handleSubmit} method='post'>
-          <div className="field">
-            <span className="fa fa-user"></span>
-            <input type="email" onChange={onChange} name='email' required placeholder="Email or username"/>
-          </div>
-          <div className="field space">
-            <span className="fa fa-lock"></span>
-            <input type="password" onChange={onChange} name='password' className="pass-key" required placeholder="Password"/>
-          </div>
-          <div className="pass">
-            <p>Forgot Password?</p>
-          </div>
-          <div className="field">
-            <button type="submit"
-                    value="Login"  >Login </button>
-          </div>
-        </form>
-        <div className="signup">Don't have account?
-          <a href="/">Signup Now</a>
-        </div>
+    <div>
+      <div style={{ marginTop: '80px' }}>
+      {(
+        () => {
+          if (state === true) {
+
+            return (<>
+              <div class="alert alert-success alert-dismissible" role="alert" Style="background-color:green; color:white;">
+                You are <strong>Successfully</strong> logged in!!
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+            </>)
+
+          }
+        }
+      )()}
+        {/* <Heading/> */}
+
       </div>
+
+      <body>
+
+        <div className="bg-img">
+          <div className="content">
+            <header style={{ color: 'black' }}>Login</header>
+            <form onSubmit={handleSubmit} method='post'>
+              <div className="field">
+                <span className="fa fa-user"></span>
+                <input type="email" onChange={onChange} name='email' required placeholder="Email or username" />
+              </div>
+              <div className="field space">
+                <span className="fa fa-lock"></span>
+                <input type="password" onChange={onChange} name='password' className="pass-key" required placeholder="Password" />
+              </div>
+              <div className="pass">
+                <p>Forgot Password?</p>
+              </div>
+              <div className="field">
+                <button type="submit"
+                  value="Login"  >Login </button>
+              </div>
+            </form>
+            <div className="signup">Don't have account?
+              <a href="/">Signup Now</a>
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </body>
+
+
+
+
+
     </div>
 
-    
-
-
-  </body>
-
-
-			
-			
-    
-  </div>
-
-	)
+  )
 }
 
 export default Login
