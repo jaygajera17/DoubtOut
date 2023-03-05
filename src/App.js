5ef867c850139e8d76c8d0dd231eba68a99f7ee5
pavangadhiya <pavangadhiya06@gmail.com>
2023-03-05 10:48:44 +0530

    profile position fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Editor from './components/Editor/Editor';
--- src/components/charts/Chart.js
+++ src/components/charts/Chart.js
@@ -3,60 +3,69 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import parse from 'html-react-parser';
-import Profile from '../Profile/Profile';
 import { PieChart } from 'react-minimal-pie-chart';
+import ProfileSidebar from '../ProfileSidebar/ProfileSidebar';
+import ProfileHeader from '../ProfileHeader/ProfileHeader';
+
 import './chart.css'
 
 export default function Chart() {
     return (
         <div>
-            <Profile />
-            <div className='allChart'>
-                <div className='QuesChart'>
-                    <div className='title'>No of asked questions</div>
+            <div className="container" Style="height:100vh; margin-top:13vh; z-index:1; background-color:white">
+                <ProfileSidebar />
+                <div className='header_and_content'>
+                    <ProfileHeader />
 
-                    <div>
-                        <PieChart lineWidth='60' viewBoxSize={[100, 100]}
-                            data={[
-                                { title: 'React', value: 10, color: '#E38627' },
-                                { title: 'Node', value: 25, color: '#C13C37' },
-                                { title: 'Mongo', value: 5, color: '#6A2135' },
-                            ]}
-                        />
+                    <div className='allChart'>
+                        <div className='QuesChart'>
+                            <div className='title'>No of asked questions</div>
+
+                            <div>
+                                <PieChart lineWidth='60' viewBoxSize={[100, 100]}
+                                    data={[
+                                        { title: 'React', value: 10, color: '#E38627' },
+                                        { title: 'Node', value: 25, color: '#C13C37' },
+                                        { title: 'Mongo', value: 5, color: '#6A2135' },
+                                    ]}
+                                />
+                            </div>
+                        </div>
+
+                        <div className='QuesChart'>
+                            <div className='title'>No of given answers.</div>
+
+                            <div>
+                                <PieChart lineWidth='60' viewBoxSize={[100, 100]}
+                                    data={[
+                                        { title: 'MERN', value: 35, color: '#E38627' },
+                                        { title: 'Node', value: 25, color: '#C13C37' },
+                                        { title: 'Mongo', value: 5, color: '#6A2135' },
+                                    ]}
+                                />
+
+                            </div>
+                        </div>
+
+                        <div className='QuesChart'>
+                            <div className='title'>No of accepted answers</div>
+
+                            <div>
+                                <PieChart lineWidth='60' viewBoxSize={[100, 100]}
+                                    data={[
+                                        { title: 'React', value: 5, color: '#E38627' },
+                                        { title: 'Node', value: 5, color: '#C13C37' },
+                                        { title: 'Mongo', value: 55, color: '#6A2135' },
+                                    ]}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div className='QuesChart'>
-                    <div className='title'>No of given answers.</div>
+                    <hr Style="border: 0.7px solid" />
 
-                    <div>
-                        <PieChart lineWidth='60' viewBoxSize={[100, 100]}
-                            data={[
-                                { title: 'MERN', value: 35, color: '#E38627' },
-                                { title: 'Node', value: 25, color: '#C13C37' },
-                                { title: 'Mongo', value: 5, color: '#6A2135' },
-                            ]}
-                        />
-
-                    </div>
-                </div>
-
-                <div className='QuesChart'>
-                    <div className='title'>No of accepted answers</div>
-
-                    <div>
-                        <PieChart lineWidth='60' viewBoxSize={[100, 100]}
-                            data={[
-                                { title: 'React', value: 5, color: '#E38627' },
-                                { title: 'Node', value: 5, color: '#C13C37' },
-                                { title: 'Mongo', value: 55, color: '#6A2135' },
-                            ]}
-                        />
-                    </div>
                 </div>
             </div>
-
-            <hr Style="border: 0.7px solid" />
         </div>
     );
 }
