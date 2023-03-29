69344ccf469149b6be2a3b5a8de897d894b6ffab
pavangadhiya <pavangadhiya06@gmail.com>
2023-03-29 12:17:34 +0530

    miscellaneous1

--- src/components/Admin/AdminSidebar.js
+++ src/components/Admin/AdminSidebar.js
@@ -17,15 +17,12 @@ export default function AdminSidebar() {
                     <div className="sidebar-option">
                         <NavLink className="sideLink" to="/Adminquestions">Questions</NavLink>
                     </div>
-<<<<<<< HEAD
-=======
                     <div className="sidebar-option">
                         <NavLink  className="sideLink" to="/Adminanswer">answer</NavLink>
                     </div>
                     {/* <div className="sidebar-option">
                         <NavLink  className="sideLink">Badges</NavLink>
                     </div> */}
->>>>>>> 338075220802a74b62b3189b746aaeae0c69b27b
                 </div>
             </div>
         </div>
